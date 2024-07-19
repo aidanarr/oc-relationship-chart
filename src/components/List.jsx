be7474266bcc21c45c1abaa4131da876c963c9d2
aidanar <aidanarfer@outlook.es>
2024-07-19 12:41:53 +0200

    change position success

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -9,29 +9,60 @@ const List = () => {
 
     const [array, setArray] = useState(datacopy)
     const [isClicked, setIsClicked] = useState()
+    const [top, setTop] = useState()
+    const [left, setLeft] = useState()
 
-    useEffect(() => {
-      if (elem.current) {
-        const rect = elem.current.getBoundingClientRect();
-        // You can now use rect to position other elements or for any other purpose
-        console.log(rect.width)
-      }
-    }, []);
+    // useEffect(() => {
+    //   if (elem.current) {
+    //     const rect = elem.current.getBoundingClientRect();
+    //     setTop(rect.top)
+    //     setLeft(rect.left)
+    //     // You can now use rect to position other elements or for any other purpose
+    //     console.log(rect.top)
+    //   }
+    // }, [isClicked]);
 
     function handleClick(ev) {
         const id = parseInt(ev.target.id);
+        const rect = ev.target.getBoundingClientRect();
         setIsClicked(id)
-        setArray(array)
+        setTop(rect.top)
+        setLeft(rect.left)
     }
     
     function renderList() {
         return array.map((chara, i) => {
-            return <div ref={isClicked === chara.id ? elem : null} onClick={handleClick} className={`charabox ${isClicked === chara.id ? "clickedChara" : ""}`} id={chara.id} key={i}><p>{chara.name}</p></div>
+            return <div onClick={handleClick} className={`charabox ${isClicked === chara.id ? "clickedChara" : ""}`} id={chara.id} key={i}><p>{chara.name}</p></div>
             })
     }
 
   return (
     <>
+    <style>
+      {`
+        .clickedChara {
+          position: absolute;
+          top: ${top}px;
+          left: ${left}px;
+          animation: 1s clicked 2s ease-in-out forwards;
+         }
+        
+         @keyframes clicked {
+          0% {
+            top: ${top}px;
+            left: ${left}px;
+        
+          }
+        
+          100% {
+        
+            top: 50px;
+            left: 40px;
+          }
+        
+        `
+      }
+    </style>
     <div>
       <div className="ref"></div>
       <div className="div">
