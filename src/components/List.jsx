6868343853a741056d6425efeeb94f49beb14b50
aidanar <aidanarfer@outlook.es>
2024-07-19 12:20:13 +0200

    update test

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,22 @@
 import datacopy from "../services/data-copy.json"
 import { useState } from "react"
+import { useRef } from "react"
+import { useEffect } from "react"
 
 const List = () => {
 
+    const elem = useRef(null)
+
     const [array, setArray] = useState(datacopy)
     const [isClicked, setIsClicked] = useState()
+
+    useEffect(() => {
+      if (elem.current) {
+        const rect = elem.current.getBoundingClientRect();
+        // You can now use rect to position other elements or for any other purpose
+        console.log(rect.width)
+      }
+    }, []);
 
     function handleClick(ev) {
         const id = parseInt(ev.target.id);
@@ -14,14 +26,19 @@ const List = () => {
     
     function renderList() {
         return array.map((chara, i) => {
-            return <div onClick={handleClick} className={`charabox ${isClicked === chara.id ? "clickedChara" : ""}`} id={chara.id} key={i}><p>{chara.name}</p></div>
+            return <div ref={isClicked === chara.id ? elem : null} onClick={handleClick} className={`charabox ${isClicked === chara.id ? "clickedChara" : ""}`} id={chara.id} key={i}><p>{chara.name}</p></div>
             })
     }
 
   return (
+    <>
     <div>
-        {renderList()}
+      <div className="ref"></div>
+      <div className="div">
+          {renderList()}
+      </div>
     </div>
+    </>
   )
 }
 
